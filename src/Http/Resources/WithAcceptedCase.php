6ede38041b451f2f5c039260a1bfd44cdb41ff94
Craig Smith <craig.smith@customd.com>
2021-08-12 10:14:28 +1200

    fix: :bug: Error in the case mapping
    #3

--- src/Http/Resources/WithAcceptedCase.php
+++ src/Http/Resources/WithAcceptedCase.php
@@ -9,8 +9,8 @@ trait WithAcceptedCase
     protected function toAcceptCase($request, $data)
     {
         return match (Str::lower($request->header('X-Accept-Case-Type'))) {
-            'camel','camel-case' => collect($data)->snakeKeys(true)->toArray(),
-            'snake','snake-case' => collect($data)->camelKeys(true)->toArray(),
+            'camel','camel-case' => collect($data)->camelKeys(true)->toArray(),
+            'snake','snake-case' => collect($data)->snakeKeys(true)->toArray(),
             'kebab', 'kebab-case' => collect($data)->kebabKeys(true)->toArray(),
             default => $data
         };
